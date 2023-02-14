66257ab57e10ecbaa43897161f12f96c052bd097
TEGAR_71105 <tegardito02@gmail.com>
2023-02-14 21:49:01 +0700

    test

--- batiku.sh
+++ batiku.sh
@@ -2,11 +2,21 @@
 
 # Update the package list and install Apache2, PHP, PHP MySQLi, Git, and MariaDB
 apt-get update -y
-apt-get install apache2 php php-mysqli php-mysql mariadb-server -y
+apt-get install apache2 php php-mysqli php-mysql git mariadb-server -y
 clear 
 
 # Start Apache2 service
 service apache2 start
+
+# Clone the web-dinamis-produktif repository to /var/www/
+
+cd /var/www/ && mkdir tes
+chmod 777 -R /var/www/tes/
+cd /var/www/tes/ && git clone https://github.com/OmTegar/batiku.git
+cd /var/www/batiku/admin/proses/image/ && mv -f /var/www/tes/batiku/admin/proses/image/* /var/www/batiku/admin/proses/image/
+cd /var/www/batiku/admin/proses/ && mv -f /var/www/tes/batiku/admin/proses/* /var/www/batiku/admin/proses/
+cd /var/www/batiku/admin/ && mv -f /var/www/tes/batiku/admin/* /var/www/batiku/admin/
+cd /var/www/batiku/ && mv -f /var/www/tes/batiku/* /var/www/batiku/
 
 # Give permission to access asset directory and index.php file
 chmod 777 -R /var/www/batiku/
@@ -23,7 +33,7 @@ cp 000-default.conf ../sites-enabled/
 systemctl restart apache2
 
 # Replace the contents of the file
-sed -i 's/localhost/database-1.cgu4ysargwic.us-east-1.rds.amazonaws.com/' /var/www/batiku/config/db.php
+sed -i 's/localhost/database-1.csfoslsqwvcb.us-east-1.rds.amazonaws.com/' /var/www/batiku/config/db.php
 sed -i 's/root/admin/' /var/www/batiku/config/db.php
 sed -i 's/\"\"/\"admin123\"/g' /var/www/batiku/config/db.php
 
@@ -35,7 +45,7 @@ else
 fi
 
 # Login to the RDS database
-mysql -h database-1.cgu4ysargwic.us-east-1.rds.amazonaws.com -u admin -p << EOF
+mysql -h database-1.csfoslsqwvcb.us-east-1.rds.amazonaws.com -u admin -p << EOF
 
 # Show existing databases
 show databases;
@@ -70,3 +80,4 @@ EOF
 nano /var/www/batiku/config/db.php
 nano /etc/apache2/sites-available/000-default.conf
 nano /etc/apache2/sites-enabled/000-default.conf
+systemctl reboot
